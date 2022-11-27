b3c73af0b29043faad767490200b96a7c4ca523a
bhoff <bruce.hoff@sagebase.org>
2022-11-27 08:16:30 -0800

    reduce CPU from 2 to 1; app' version 0.10

--- cdk/docker_fargate/docker_fargate_stack.py
+++ cdk/docker_fargate/docker_fargate_stack.py
@@ -111,10 +111,10 @@ class DockerFargateStack(Stack):
         load_balanced_fargate_service = ecs_patterns.\
                 ApplicationLoadBalancedFargateService(self, get_service_name(),
             cluster=cluster,            # Required
-            cpu=2048,                    # Default is 256 (=.25 vCPU)
+            cpu=1024,                    # Default is 256 (=.25 vCPU)
             desired_count=1,            # Number of copies of the 'task' (i.e. the app') running behind the ALB
             task_image_options=task_image_options,
-            memory_limit_mib=4096,      # Default is 512 (4096 for 4GB memory which goes with 2048 cpu (=2 vCPU))
+            memory_limit_mib=2048,      # Default is 512 (4096 for 4GB memory which goes with 2048 cpu (=2 vCPU))
             public_load_balancer=True,  # Default is False
             # TLS:
             protocol=elbv2.ApplicationProtocol.HTTPS,
